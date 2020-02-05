6ed1762f1b399c6b24ce0bfd077f1dcd268a1dba
newtux <newtux@gmail.com>
2020-02-05 02:12:02 -0500

    Update the project

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-kindletocsv
-===========
+# Kindle Definitions to CSV
 
-Kindle Defined words to CSV
+Words you've looked up on the kindle, defined and thrown into a csv file (for later imports into Anki). 
 
-Takes highlighted kindle words, uses Automator.app to get definitions from Dictionary.app, and writes out to CSV file (which can then be imported into other programs).
+## A Note on Dictionaries 
+
+After reading [this fantstic article on dictionaries](https://jsomers.net/blog/dictionary) I've been using Webster's 1913 dictionary on my kindle. I wanted to have the same definitions in my csv. Originally I was going to parse the [Project Gutenberg Version](http://www.gutenberg.org/ebooks/29765), but luckily I found [GCIDE](https://gcide.gnu.org.ua/) which I read via [Dico](http://puszcza.gnu.org.ua/software/dico/dico.html). Opening a subprocess to read every definition is a bit slow for larger highlight lists. 
 
 ## Usage
 
-- Get vocab.db file from Kindle and put in same directory as kindledefine.py script
+- Get vocab.db file from Kindle and put copy it over somewhere
     * Located in /Volumes/Kindle/system/vocabulary/ (make sure you have show hidden files turned on)
-- Run kindledefine.py
+- `python kindledefine.py --help`
--- kindledefine.py
+++ kindledefine.py
@@ -1,20 +1,51 @@
-import sqlite3, csv, unicodedata, re
-from subprocess import *
+import csv
+import sqlite3
+import subprocess
+from pathlib import Path
 
-outputdata = []
-conn = sqlite3.connect('vocab.db')
+import click
+from tqdm import tqdm
 
-#get the data
 
-for row in conn.execute('Select * From WORDS'):
-	word = unicodedata.normalize('NFKD',row[1]).encode('ascii','ignore')
-	definition = Popen(["automator","-D","Text="+word,"define.workflow"],stdout=PIPE).communicate()[0]
-	#cleanup
-	definition = re.sub('[()]', '', definition)
-	outputdata.append([word, definition])
+def define(word: str, dictionary="gcide") -> str:
+    # word = unicodedata.normalize('NFKD',row[1]).encode('ascii','ignore')
+    definition = subprocess.run(["dico", "-d", dictionary, word], capture_output=True)
 
-#write the data 
+    return definition.stdout.decode()
 
-with open('output.csv', 'w') as csvfile:
-	writer = csv.writer(csvfile)
-	writer.writerows(outputdata)
+
+@click.command()
+@click.option(
+    "--vocab",
+    help="Path to kindle vocab.db",
+    required=True,
+    type=click.Path(exists=True, readable=True),
+)
+@click.option(
+    "--dictionary", help="dico dictionary server", default="gcide", required=True,
+)
+@click.option(
+    "-o",
+    "--ouput_file",
+    help="Path to output file",
+    required=True,
+    type=click.Path(writable=True),
+)
+def run(vocab: Path, dictionary: str, ouput_file: Path):
+    out = []
+    conn = sqlite3.connect(vocab)
+    query = "select word, stem from words where length(word)>4"  # don't care about accidentals
+    rows = conn.execute(query).fetchall()
+
+    for row in tqdm(rows):
+        definition = define(word := row[0])
+        if not definition:
+            print(f"Couldn't find a definition for {word}")
+        out.append((word, row[1]))
+    with Path(ouput_file).open() as f:
+        writer = csv.writer(f)
+        writer.writerows(out)
+
+
+if __name__ == "__main__":
+    run()
